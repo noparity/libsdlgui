3a24e4a2ae016be99c0f7531815b913fa0e9c48a
Joel Hendrix <noparity@hotmail.com>
2016-02-18 19:55:38 -0800

    Add comparison operators for Dimention class.

--- include/Dimentions.hpp
+++ include/Dimentions.hpp
@@ -8,4 +8,14 @@ struct Dimentions
     Dimentions(int w, int h) : W(w), H(h) {}
 };
 
+inline bool operator==(const Dimentions& lhs, const Dimentions& rhs)
+{
+    return lhs.W == rhs.W && lhs.H == rhs.H;
+}
+
+inline bool operator!=(const Dimentions& lhs, const Dimentions& rhs)
+{
+    return !(lhs == rhs);
+}
+
 #endif // DIMENTIONS_HPP
